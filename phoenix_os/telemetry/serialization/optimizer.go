4de745f5f07376460189dd39c88b55da92c8ede5
fallofpheonix <ujjosing@gmail.com>
2026-05-21 20:02:39 +0530

    chore: Correct go.work paths for agent modules

--- phoenix_os/telemetry/serialization/optimizer.go
+++ phoenix_os/telemetry/serialization/optimizer.go
@@ -1,11 +1,10 @@
 package serialization
 
 import (
-	"encoding/json"
-	"github.com/segmentio/encoding/json" // Utilizing high-performance JSON encoder
+	sjson "github.com/segmentio/encoding/json" // Utilizing high-performance JSON encoder
 )
 
 // OptimizedMarshaler replaces standard library JSON encoding with high-perf alternatives.
 func OptimizedMarshaler(v interface{}) ([]byte, error) {
-	return json.Marshal(v)
+	return sjson.Marshal(v)
 }
